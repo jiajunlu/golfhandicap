bec533af7fe092aa962fdf9a413f2be3f9f36c5a
Jiajun Lu <jiajunlu@gmail.com>
2016-03-29 16:51:47 -0400

    remove database info from settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -94,12 +94,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'golfhandicap',
-        # The following settings are not used with sqlite3:
-        'USER': 'dbuser',
-        'PASSWORD': 'dbpass',
-        'HOST': 'localhost',             # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
     }
 }
 
