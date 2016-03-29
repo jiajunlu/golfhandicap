8bc2f094364387d0ffa15c116ec3ecedc6a4ff7f
Jiajun Lu <jiajunlu@gmail.com>
2016-03-29 16:41:23 -0400

    added local_settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,21 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+
+
+DATABASES = { 'default' : dj_database_url.config()}
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# try to load local_settings.py if it exists
+try:
+      from local_settings import *
+except Exception as e:
+      pass
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
