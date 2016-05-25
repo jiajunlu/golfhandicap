c4adf8abdc9d2807616d59381abe9d3556ac6c0b
Jiajun Lu <jiajunlu@gmail.com>
2016-05-25 15:53:30 -0400

    add rules

--- golfhandicap/urls.py
+++ golfhandicap/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
+
 
 from . import views
 
@@ -6,4 +8,5 @@ urlpatterns = [
     url(r'^$', views.index, name='golfhandicap'),
     url(r'^allplayers$', views.allplayers, name='allplayers'),
     url(r'^games$', views.games, name='games'),
+    url(r'^rules$', TemplateView.as_view(template_name='golfhandicap/rules.html')),
 ]
